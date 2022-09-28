f9c32fdb88694fbec07402be516e6459ea59a513
RAKITEMBO <lewalex18@gmail.com>
2022-09-28 20:39:30 +0300

    Task 2

--- src/Lesson_3.java
+++ src/Lesson_3.java
@@ -3,39 +3,39 @@ import java.util.Scanner;
 public class Lesson_3 {
     public static void main(String[] args) {
         //    Task_1
-        System.out.println("TASK 1"+"\n");
-        String GreetingOne = "Hi type number of months. ";
-        String ExplainOne = "Where 0 - january, 11 - december:";
-        System.out.println(GreetingOne + "\n" + ExplainOne);
-        System.out.println("\n");
-        Scanner MonthOne = new Scanner(System.in);
-        int Month = MonthOne.nextInt();
-
-
-        switch (Month) {
-            case 0:
-            case 1:
-            case 2:
-                System.out.println("Time of the year - Winter");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Time of the year - Spring");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("Time of the year - Summer");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("Time of the year - Autumn");
-                break;
-            default:
-                System.out.println("Be careful!!!");
-        }
+//        System.out.println("TASK 1"+"\n");
+//        String GreetingOne = "Hi type number of months. ";
+//        String ExplainOne = "Where 0 - january, 11 - december:";
+//        System.out.println(GreetingOne + "\n" + ExplainOne);
+//        System.out.println("\n");
+//        Scanner MonthOne = new Scanner(System.in);
+//        int MonthTask1 = MonthOne.nextInt();
+//
+//
+//        switch (MonthTask1) {
+//            case 0:
+//            case 1:
+//            case 11:
+//                System.out.println("Time of the year - Winter");
+//                break;
+//            case 2:
+//            case 3:
+//            case 4:
+//                System.out.println("Time of the year - Spring");
+//                break;
+//            case 5:
+//            case 6:
+//            case 7:
+//                System.out.println("Time of the year - Summer");
+//                break;
+//            case 8:
+//            case 9:
+//            case 10:
+//                System.out.println("Time of the year - Autumn");
+//                break;
+//            default:
+//                System.out.println("Be careful!!!");
+//        }
 
         //***
         // Есть ли возможность написать в операторе switch case условие равентства?
@@ -46,5 +46,24 @@ public class Lesson_3 {
         //***
 
 
+////    Task_2
+//        System.out.println("TASK 2" + "\n");
+//        String GreetingTwo = "Hi type number of months. ";
+//        String ExplainTwo = "Where 0 - january, 11 - december:";
+//        System.out.println(GreetingTwo + "\n" + ExplainTwo);
+//        System.out.println("\n");
+//        Scanner MonthTwo = new Scanner(System.in);
+//        int MonthTaskTwo = MonthTwo.nextInt();
+//        if (MonthTaskTwo <= 1 || MonthTaskTwo == 11) {
+//            System.out.println("Time of the year - Winter");
+//        } else if (MonthTaskTwo <=4) {
+//            System.out.println("Time of the year - Spring");
+//        } else if (MonthTaskTwo <= 7) {
+//            System.out.println("Time of the year - Summer");
+//        } else if (MonthTaskTwo <= 10) {
+//            System.out.println("Time of the year - Autumn");
+//        } else {
+//            System.out.println("We haven't this month in condition");
+//        }
     }
 }
