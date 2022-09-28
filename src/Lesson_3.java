90407f229a4c0e1b7f8664cce982c46d8a560cd7
RAKITEMBO <lewalex18@gmail.com>
2022-09-28 21:33:54 +0300

    Task 3-4

--- src/Lesson_3.java
+++ src/Lesson_3.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class Lesson_3 {
     public static void main(String[] args) {
+
         //    Task_1
+
 //        System.out.println("TASK 1"+"\n");
 //        String GreetingOne = "Hi type number of months. ";
 //        String ExplainOne = "Where 0 - january, 11 - december:";
@@ -46,7 +48,8 @@ public class Lesson_3 {
         //***
 
 
-////    Task_2
+        //    Task_2
+
 //        System.out.println("TASK 2" + "\n");
 //        String GreetingTwo = "Hi type number of months. ";
 //        String ExplainTwo = "Where 0 - january, 11 - december:";
@@ -65,5 +68,58 @@ public class Lesson_3 {
 //        } else {
 //            System.out.println("We haven't this month in condition");
 //        }
+
+        //    Task_3
+
+//        System.out.println("TASK 3" + "\n");
+//        System.out.println("Hi write outside degree: ");
+//        System.out.println("\n");
+//        Scanner Degree = new Scanner(System.in);
+//        int T = Degree.nextInt();
+//
+//        System.out.println("Temperature :" + T);
+//        if (T > -5) {
+//            System.out.println("Warm");
+//        } else if (T <= -5 && T > -20) {
+//            System.out.println("Normal");
+//        } else if (T <= -20) {
+//            System.out.println("Freeze");
+//        }
+
+        //    Task_4
+//        System.out.println("TASK 4" + "\n");
+//        String Type = "Type color of rainbow: ";
+//        System.out.println(Type);
+//        System.out.println("1 - Red;\n2 - Orange;\n3 - Yellow;\n4 - Green;\n5 - Blue;\n6 - Indigo;\n7 - Violet;");
+//        Scanner RainBow = new Scanner(System.in);
+//        System.out.println("Write number: ");
+//        int RB = RainBow.nextInt();
+//        switch (RB){
+//            case (1):
+//                System.out.println(Type+"Red");
+//                break;
+//            case (2):
+//                System.out.println(Type+"Orange");
+//                break;
+//            case (3):
+//                System.out.println(Type+"Yellow");
+//                break;
+//            case (4):
+//                System.out.println(Type+"Green");
+//                break;
+//            case (5):
+//                System.out.println(Type+"Blue");
+//                break;
+//            case (6):
+//                System.out.println(Type+"Indigo");
+//                break;
+//            case (7):
+//                System.out.println(Type+"Violet");
+//                break;
+//            default:
+//                System.out.println("Read conditions!");
+
+        }
+
     }
 }
